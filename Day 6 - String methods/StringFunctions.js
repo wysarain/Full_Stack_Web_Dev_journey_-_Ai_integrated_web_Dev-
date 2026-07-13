c071e739e533404a8a044a247002fec0d85d6c0c
Awais Afzal <ChoudharyAwais942@gmail.com>
2026-07-13 11:35:25 +0500

    Day 6 - String methods

---
+++ Day 6 - String methods/StringFunctions.js
@@ -0,0 +1,57 @@
+// All string methods 
+
+let name="Ali";
+
+console.log(name[0]);
+
+console.log(name.length);
+
+let s="hello";
+
+console.log(s.toUpperCase());
+
+ console.log("HELLO".toLowerCase());
+
+ let s=" hello ";
+
+console.log(s.trim());
+
+let s="JavaScript";
+
+console.log(s.includes("Script"));
+
+console.log(s.startsWith("Java"));
+
+console.log(s.endsWith("ipt"));
+
+console.log(s.endsWith("ipt"));
+
+console.log(s.indexOf("S"));
+
+console.log("banana".lastIndexOf("a"));
+
+let s="JavaScript";
+
+console.log(s.slice(0,4));
+
+console.log(s.slice(-6));
+
+console.log(s.substring(0,4));
+ 
+console.log("I like Java".replace("Java","JS"));
+console.log("a a a".replaceAll("a","b"));
+
+
+let s="Apple,Banana,Mango";
+
+console.log(s.split(","));
+ 
+
+console.log(s.charAt(2));
+
+console.log("Hi ".repeat(3));
+
+let name="Ali";
+
+console.log(`Hello ${name}`);
+
